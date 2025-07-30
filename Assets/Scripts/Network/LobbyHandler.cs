b04f39eaed675d461b64e892f515c2ffcea7a104
edebaze <e.debaze@gmail.com>
2025-07-30 12:16:05 +0200

    clean bots spawn in lobby

--- Assets/Scripts/Network/LobbyHandler.cs
+++ Assets/Scripts/Network/LobbyHandler.cs
@@ -257,13 +257,17 @@ namespace Network
                             playerData.SetBuild(ProgressionCloudData.CurrentArena.HasBuildData() ? ProgressionCloudData.CurrentArena.BuildData : CharacterBuildsCloudData.CurrentBuild);
                         }
 
+                        // send self data to the GameManager
                         GameManager.Instance.AddPlayerDataServerRPC(
                             NetworkManager.Singleton.LocalClientId,
                             playerData
                         );
 
-                        SendBotsData();
+                        // if has to fill game with bots - send bots data
+                        if (IsHost && m_FillWithBots)
+                            SendBotsData();
                     
+                        // go to next state
                         NextState();
                         return;
 
