16f71eb0ba32684d35d2cef213cd83d108822677
edebaze <e.debaze@gmail.com>
2025-12-09 17:42:39 +0100

    fix update manager bug

--- Assets/Scripts/Managers/UpdateManager.cs
+++ Assets/Scripts/Managers/UpdateManager.cs
@@ -44,7 +44,7 @@ namespace Assets.Scripts.Managers
                 return;
             }
 
-            while (CurrentVersion != GameVersion)
+            while (CurrentVersion < GameVersion)
             {
                 if (! UpdateVersion())
                 {
